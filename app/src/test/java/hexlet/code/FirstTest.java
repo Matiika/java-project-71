95ea9cf8d126cf59ff1f5ae11c51d0b6d32b05d5
truemat <matvei.truhan@gmail.com>
2024-06-04 20:47:53 +0300

    Update

--- app/src/test/java/hexlet/code/FirstTest.java
+++ app/src/test/java/hexlet/code/FirstTest.java
@@ -2,6 +2,7 @@ package hexlet.code;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -9,12 +10,15 @@ import java.nio.file.Paths;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FirstTest {
+    public static String fileToString(String filePathString) throws IOException {
+        Path filePath = Paths.get(filePathString);
+        return Files.readString(filePath).trim();
+    }
 
     @Test
     public void testJsonCompareStylish() throws Exception {
         String actual = Differ.generate("./src/test/resources/file1.json", "./src/test/resources/file2.json");
-        Path filePath = Paths.get("./src/test/resources/fixtures/resultTestJsonStylish.txt");
-        String expected = Files.readString(filePath).trim();
+        String expected = fileToString("./src/test/resources/fixtures/resultTestJsonStylish.txt");
         assertEquals(actual, expected);
     }
 
@@ -22,16 +26,14 @@ public class FirstTest {
     public void testJsonComparePlain() throws Exception {
         String actual = Differ.generate("./src/test/resources/file1.json",
                 "./src/test/resources/file2.json", "plain");
-        Path filePath = Paths.get("./src/test/resources/fixtures/resultTestJsonPlain.txt");
-        String expected = Files.readString(filePath).trim();
+        String expected = fileToString("./src/test/resources/fixtures/resultTestJsonPlain.txt");
         assertEquals(actual, expected);
     }
 
     @Test
     public void testYMLCompareStylish() throws Exception {
         String actual = Differ.generate("./src/test/resources/file1.yml", "./src/test/resources/file2.yml");
-        Path filePath = Paths.get("./src/test/resources/fixtures/resultTestYMLStylish.txt");
-        String expected = Files.readString(filePath).trim();
+        String expected = fileToString("./src/test/resources/fixtures/resultTestYMLStylish.txt");
         assertEquals(actual, expected);
     }
 
@@ -39,8 +41,7 @@ public class FirstTest {
     public void testYMLComparePlain() throws Exception {
         String actual = Differ.generate("./src/test/resources/file1.yml",
                 "./src/test/resources/file2.yml", "plain");
-        Path filePath = Paths.get("./src/test/resources/fixtures/resultTestYMLPlain.txt");
-        String expected = Files.readString(filePath).trim();
+        String expected = fileToString("./src/test/resources/fixtures/resultTestYMLPlain.txt");
         assertEquals(actual, expected);
     }
 }
